1d3dbc4c542d59c7821c91b77922c716872f13ff
Steve <swatarianess@gmail.com>
2016-01-28 16:36:40 +0100

    W4Q2

---
+++ src/com/week4/Q2/Palindrome.java
@@ -0,0 +1,33 @@
+package com.week4.Q2;
+
+import java.io.PrintStream;
+import java.util.Objects;
+import java.util.Scanner;
+
+/**
+ * Created by swatarianess on 28/01/2016.
+ */
+public class Palindrome {
+
+    private static final PrintStream ps = System.out;
+    private static final Scanner in = new Scanner(System.in);
+    private static String wordToTest = "";
+
+    public Palindrome(){
+
+    }
+
+    public static void main(String[] args){
+        ps.println("##### Palindrom Check #####");
+        ps.println("Please enter a word: ");
+        ps.println("");
+        ps.println("Input is a " + isPalindrome(in.nextLine()) + " palindrome");
+    }
+
+    public static boolean isPalindrome(String sentence){
+        StringBuilder s = new StringBuilder();
+        s.append(sentence).reverse();
+        return Objects.equals(s.toString(), sentence);
+    }
+
+}
